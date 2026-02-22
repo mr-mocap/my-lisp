baf2490fd41000a07c088656f20118fbd66c152b
Template Janitor <template.janitor@example.com>
2026-02-22 22:05:14 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(my_lisp_supports_sanitizers)
   # Emscripten doesn't support sanitizers
   if(EMSCRIPTEN)
     set(SUPPORTS_UBSAN OFF)
@@ -58,110 +58,110 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(my_lisp_setup_options)
+  option(my_lisp_ENABLE_HARDENING "Enable hardening" ON)
+  option(my_lisp_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    my_lisp_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    my_lisp_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  my_lisp_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR my_lisp_PACKAGING_MAINTAINER_MODE)
+    option(my_lisp_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(my_lisp_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(my_lisp_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(my_lisp_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(my_lisp_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(my_lisp_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(my_lisp_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(my_lisp_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(my_lisp_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(my_lisp_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(my_lisp_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(my_lisp_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(my_lisp_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(my_lisp_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(my_lisp_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(my_lisp_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(my_lisp_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(my_lisp_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(my_lisp_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(my_lisp_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(my_lisp_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(my_lisp_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(my_lisp_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(my_lisp_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      my_lisp_ENABLE_IPO
+      my_lisp_WARNINGS_AS_ERRORS
+      my_lisp_ENABLE_SANITIZER_ADDRESS
+      my_lisp_ENABLE_SANITIZER_LEAK
+      my_lisp_ENABLE_SANITIZER_UNDEFINED
+      my_lisp_ENABLE_SANITIZER_THREAD
+      my_lisp_ENABLE_SANITIZER_MEMORY
+      my_lisp_ENABLE_UNITY_BUILD
+      my_lisp_ENABLE_CLANG_TIDY
+      my_lisp_ENABLE_CPPCHECK
+      my_lisp_ENABLE_COVERAGE
+      my_lisp_ENABLE_PCH
+      my_lisp_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  my_lisp_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (my_lisp_ENABLE_SANITIZER_ADDRESS OR my_lisp_ENABLE_SANITIZER_THREAD OR my_lisp_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(my_lisp_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(my_lisp_global_options)
+  if(my_lisp_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    my_lisp_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  my_lisp_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(my_lisp_ENABLE_HARDENING AND my_lisp_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR my_lisp_ENABLE_SANITIZER_UNDEFINED
+       OR my_lisp_ENABLE_SANITIZER_ADDRESS
+       OR my_lisp_ENABLE_SANITIZER_THREAD
+       OR my_lisp_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${my_lisp_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${my_lisp_ENABLE_SANITIZER_UNDEFINED}")
+    my_lisp_enable_hardening(my_lisp_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(my_lisp_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(my_lisp_warnings INTERFACE)
+  add_library(my_lisp_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  my_lisp_set_project_warnings(
+    my_lisp_warnings
+    ${my_lisp_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
@@ -172,66 +172,66 @@ macro(myproject_local_options)
 
   if(NOT EMSCRIPTEN)
     include(cmake/Sanitizers.cmake)
-    myproject_enable_sanitizers(
-      myproject_options
-      ${myproject_ENABLE_SANITIZER_ADDRESS}
-      ${myproject_ENABLE_SANITIZER_LEAK}
-      ${myproject_ENABLE_SANITIZER_UNDEFINED}
-      ${myproject_ENABLE_SANITIZER_THREAD}
-      ${myproject_ENABLE_SANITIZER_MEMORY})
+    my_lisp_enable_sanitizers(
+      my_lisp_options
+      ${my_lisp_ENABLE_SANITIZER_ADDRESS}
+      ${my_lisp_ENABLE_SANITIZER_LEAK}
+      ${my_lisp_ENABLE_SANITIZER_UNDEFINED}
+      ${my_lisp_ENABLE_SANITIZER_THREAD}
+      ${my_lisp_ENABLE_SANITIZER_MEMORY})
   endif()
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(my_lisp_options PROPERTIES UNITY_BUILD ${my_lisp_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(my_lisp_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      my_lisp_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(my_lisp_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    my_lisp_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(my_lisp_ENABLE_CLANG_TIDY)
+    my_lisp_enable_clang_tidy(my_lisp_options ${my_lisp_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(my_lisp_ENABLE_CPPCHECK)
+    my_lisp_enable_cppcheck(${my_lisp_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(my_lisp_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    my_lisp_enable_coverage(my_lisp_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(my_lisp_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(my_lisp_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(my_lisp_ENABLE_HARDENING AND NOT my_lisp_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR my_lisp_ENABLE_SANITIZER_UNDEFINED
+       OR my_lisp_ENABLE_SANITIZER_ADDRESS
+       OR my_lisp_ENABLE_SANITIZER_THREAD
+       OR my_lisp_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    my_lisp_enable_hardening(my_lisp_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
--- cmake/LibFuzzer.cmake
+++ cmake/LibFuzzer.cmake
@@ -1,4 +1,4 @@
-function(myproject_check_libfuzzer_support var_name)
+function(my_lisp_check_libfuzzer_support var_name)
   set(LibFuzzerTestSource
       "
 #include <cstdint>
